8ddb1f4a2915c82431c723fc761534724a75ade2
Antony <119943005+AntonyL-prog@users.noreply.github.com>
2023-08-09 01:14:25 +0200

    Update MyEntity.cs
    Added Chat Entry and English Subtiles

--- myentity.melongamer/code/MyEntity.cs
+++ myentity.melongamer/code/MyEntity.cs
@@ -13,17 +13,16 @@ using Sandbox.Internal;
 using Sandbox.UI.Construct;
 
 namespace Sandbox;
-
-//Classe de mon entité, dérivé d'un parent animatedentity et IUSE
+//Class of my Entity, subclass of animatedEntity and IUSE
 internal class MyEntity : AnimatedEntity, IUse
 {
-    //Méthode de mon entité, qui fait apparaitre grace a base qui est une référence de AnimatedEntity ma classe
-    public MyEntity()
+    //Method constructor of my entity that spawn with base that derive from AnimatedEntity
+    public  MyEntity()
     {
         base.Spawn();
         UseAnimGraph = true;
     }
-    //Méthode qui permet de définire mes attributs de mon entité ainsi que la physique
+    //Method that specify attributs of my entity and his physic
     public override void Spawn()
     {
         // Always network this entity to all clients
@@ -32,33 +31,42 @@ internal class MyEntity : AnimatedEntity, IUse
         SetModel("models/sbox_props/watermelon/watermelon.vmdl_c");
         SetupPhysicsFromModel(PhysicsMotionType.Dynamic);
     }
-    //Fonction de l'API pour que quand j'écris Melon dans la console, je fais apparaitre mon melon
+    //For Admin if they want to spawn it manually
     [ConCmd.Admin("Melon")]
-    //Création de mon instance dans une méthode qui permet de spawn le melon en l'affiliant à ma classe
+    //Method that create an instance of my entity and make spawn the entity
     static void Test()
     {
         var entity = new MyEntity();
         entity.Spawn();
         //Vector3(0,0,0);
     }
-    //Si l'entité est utilisé(dans cas la "E" je retourne true et j'affiche mon contenue
+    //If my entity is used with "E" I make an instance of my entity, and make another entity on the position of the last entity
     public bool OnUse(Entity user)
     {
+
         var position = base.Position;
         MyEntity entité = new MyEntity();
         entité.SetModel("models/sbox_props/watermelon/watermelon.vmdl_c");
         entité.SetupPhysicsFromModel(PhysicsMotionType.Dynamic);
+        var steamid = 76561198049395102;
+        Message("Melon", "Spawned", steamid);
         entité.Position = position;
         return true;
     }
+    //Don't worry about this
     public bool IsUsable(Entity user)
     {
         return true;
     }
     //public static void Vector3(float x , float y, float z)
-   // {
-   //     MyEntity.Position = MyEntity.Position + (Vector3.up * 100);
-   // }
-
-
+    // {
+    //     MyEntity.Position = MyEntity.Position + (Vector3.up * 100);
+    // }
+    
+    //Method that say something in the chat
+    public static void Message(string name, string message,long PlayerId)
+        {
+          Chat.AddChatEntry(name, message, PlayerId);
+          Chat.Say(message);
+        }
 }
